b4318e67905053ab0bbc73d23bf63980086bcd83
Lil-Gepi <brandonchen2932@outlook.com>
2020-05-19 17:07:39 +0200

    update with quast

--- 08_GenAssEva_Quast_4Canu/code/quast_qssembly_evaluation_canu.sh
+++ 08_GenAssEva_Quast_4Canu/code/quast_qssembly_evaluation_canu.sh
@@ -2,7 +2,7 @@
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 02:00:00
+#SBATCH -t 00:10:00
 #SBATCH -J 08_Assembly_Evaluation_Canu_YiwenChen
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user yiwen.chen.7866@student.uu.se
@@ -17,4 +17,4 @@ module load quast
 #Settings for trimmming paired end reads
 
 #consider where to get the reference data
-quast.py /home/yich7866/02_GenAssem_Canu_PacBio/code/Canu_Output_PacBio/Enterococci.contigs.fasta  -o ../result
+quast.py /home/yich7866/02_GenAssem_Canu_PacBio/code/Canu_Output_PacBio/Enterococci.contigs.fasta /home/yich7866/GCF_001750885.1_ASM175088v1_genomic.fna -o ../result --gene-finding
--- 09_GenAssEva_Quast_4Spades/code/quast_qssembly_evaluation_spades.sh
+++ 09_GenAssEva_Quast_4Spades/code/quast_qssembly_evaluation_spades.sh
@@ -2,7 +2,7 @@
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 02:00:00
+#SBATCH -t 00:10:00
 #SBATCH -J 09_Assembly_Evaluation_Spades_YiwenChen
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user yiwen.chen.7866@student.uu.se
@@ -17,4 +17,4 @@ module load quast
 #Settings for trimmming paired end reads
 
 #consider where to get the reference data
-quast.py /home/yich7866/03_GenAssem_Spades/code/result/contigs.fasta  -o ../result
+quast.py /home/yich7866/03_GenAssem_Spades/code/result/contigs.fasta /home/yich7866/GCF_001750885.1_ASM175088v1_genomic.fna -o ../result --gene-finding
